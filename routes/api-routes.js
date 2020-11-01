5b6db945ec2eea491a3c2e1702e9b774522e5781
Ana Luna <luna.ana92@gmail.com>
2020-11-01 00:32:59 -0700

    deploying to heroku

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,6 +22,27 @@ module.exports = function (app) {
         });
     });
 
+    //Create a workout
+    app.post("/api/workouts", ({ body }, res) => {
+
+        db.Workout.create(body).then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+    
+    //Dashboard
+    app.get("/api/workouts/range", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+    });
+
     //Add exercise
     app.put("/api/workouts/:id", (req, res) => {
 
@@ -39,24 +60,5 @@ module.exports = function (app) {
 
     });
 
-    //Create a workout
-    app.post("/api/workouts", ({ body }, res) => {
 
-        db.Workout.create(body).then((dbWorkout => {
-            res.json(dbWorkout);
-        })).catch(err => {
-            res.json(err);
-        });
-    });
-
-    //Dashboard
-    app.get("/api/workouts/range", (req, res) => {
-
-        db.Workout.find({}).then(dbWorkout => {
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.json(err);
-        });
-
-    });
 }
--- server.js
+++ server.js
@@ -12,22 +12,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
-// Routes
+// Require Routes
 require("./routes/api-routes.js")(app);
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
-});
-
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/exercise.html"));
-});
-
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/stats.html"));
-});
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
