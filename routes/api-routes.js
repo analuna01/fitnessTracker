85ce1e9fe6a1e5a095e65841d7a00abde2ab9a28
Ana Luna <luna.ana92@gmail.com>
2020-10-31 23:45:45 -0700

    created routes for workouts and dashboard

---
+++ routes/api-routes.js
@@ -0,0 +1,62 @@
+const db = require("../models");
+
+
+module.exports = function (app) {
+
+    //Get workouts
+    app.get("/api/workouts", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            dbWorkout.forEach(workout => {
+                var total = 0;
+                workout.exercises.forEach(e => {
+                    total += e.duration;
+                });
+                workout.totalDuration = total;
+
+            });
+
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    });
+
+    //Add exercise
+    app.put("/api/workouts/:id", (req, res) => {
+
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $inc: { totalDuration: req.body.duration },
+                $push: { exercises: req.body }
+            },
+            { new: true }).then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+
+    });
+
+    //Create a workout
+    app.post("/api/workouts", ({ body }, res) => {
+
+        db.Workout.create(body).then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+
+    //Dashboard
+    app.get("/api/workouts/range", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+    });
+}
